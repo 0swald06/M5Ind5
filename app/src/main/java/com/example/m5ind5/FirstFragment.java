a323b3a0e6ce076459eabb5ffd825aa2246d7be3
Oswaldo <oswaldoalvaradolopez@gmail.com>
2023-06-18 19:47:51 -0400

    Initial commit

---
+++ app/src/main/java/com/example/m5ind5/FirstFragment.java
@@ -0,0 +1,71 @@
+package com.example.m5ind5;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.example.m5ind5.databinding.FragmentFirstBinding;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FirstFragment extends Fragment {
+
+    private FragmentFirstBinding binding;
+    private List<String> datalist = new ArrayList<>();
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = FragmentFirstBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        WordAdapter adapter = new WordAdapter(setData());
+
+        binding.recyclerview1.setAdapter(adapter);
+
+
+        binding.recyclerview1.setLayoutManager(new LinearLayoutManager(getContext()));
+        binding.recyclerview1.setHasFixedSize(true);
+
+
+
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                datalist.add("PALABRA"+ datalist.size());
+                // notoficar al adapter que insertamos datos
+
+                binding.recyclerview1.getAdapter().notifyItemInserted(datalist.size());
+                // scroll al final de la lista
+                binding.recyclerview1.smoothScrollToPosition(datalist.size());
+
+            }
+        });
+    }
+    private List<String> setData() {
+
+
+        return datalist;
+    }
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
---
+++ app/src/main/java/com/example/m5ind5/WordAdapter.java
@@ -0,0 +1,55 @@
+package com.example.m5ind5;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.m5ind5.databinding.WordItemListBinding;
+
+
+import java.util.List;
+
+
+public class WordAdapter  extends  RecyclerView.Adapter<WordAdapter.WordViewHolder>{
+
+    private List<String > mWordlist;
+
+    public WordAdapter(List<String>mWordlist){
+
+        this.mWordlist= mWordlist;
+    }
+
+    @NonNull
+    @Override
+    public WordViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        WordItemListBinding mBinding = WordItemListBinding.inflate(LayoutInflater.from(parent.getContext()),parent,false);
+        return  new WordViewHolder(mBinding);
+    }
+
+
+    @Override
+    public void onBindViewHolder(@NonNull WordViewHolder holder, int position) {
+        String element = mWordlist.get(position);
+        holder.textView.setText(element);
+    }
+
+    @Override
+    public int getItemCount() {
+
+        return mWordlist.size();
+    }
+
+    public class WordViewHolder extends RecyclerView.ViewHolder {
+        private TextView textView;
+        public WordViewHolder(@NonNull WordItemListBinding mBinding) {
+            super(mBinding.getRoot());
+            textView= mBinding.textView;
+        }
+    }
+
+
+}
